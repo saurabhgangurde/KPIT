bbc90659588d1a3f036c11b16f4a5e7b7ae3ad70
Shrey <shreypaharia@gmail.com>
2016-09-25 23:23:31 +0530

    added file moving_cars.py with animation of cars on intersection

---
+++ KPIT-master/moving_cars.py
@@ -0,0 +1,149 @@
+"""Animation of Elastic collisions with Gravity
+
+author: Jake Vanderplas
+email: vanderplas@astro.washington.edu
+website: http://jakevdp.github.com
+license: BSD
+Please feel free to use and modify this, but keep the above information. Thanks!
+"""
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+import matplotlib.animation as animation
+import pylab as pl
+from matplotlib import collections  as mc
+
+class ParticleBox:
+    """Orbits class
+    
+    init_state is an [N x 4] array, where N is the number of particles:
+       [[x1, y1, vx1, vy1],
+        [x2, y2, vx2, vy2],
+        ...               ]
+
+    bounds is the size of the box: [xmin, xmax, ymin, ymax]"""
+    
+    def __init__(self,
+                 init_state = [[1, 0, 0, -1],
+                               [-0.5, 0.5, 0.5, 0.5],
+                               [-0.5, -0.5, -0.5, 0.5]],
+                 bounds = [-2, 2, -2, 2],
+                 size = 0.04,
+                 M = 0.05,
+                 G = 9.8):
+        self.init_state = np.asarray(init_state, dtype=float)
+        self.M = M * np.ones(self.init_state.shape[0])
+        self.size = size
+        self.state = self.init_state.copy()
+        self.time_elapsed = 0
+        self.bounds = bounds
+        self.G = G
+
+    def step(self, dt):
+        """step once by dt seconds"""
+        self.time_elapsed += dt
+        
+        # update positions
+        self.state[:, :2] += dt * self.state[:, 2:]
+
+       
+        # check for crossing boundary
+        crossed_x1 = (self.state[:, 0] < self.bounds[0] + self.size)
+        crossed_x2 = (self.state[:, 0] > self.bounds[1] - self.size)
+        crossed_y1 = (self.state[:, 1] < self.bounds[2] + self.size)
+        crossed_y2 = (self.state[:, 1] > self.bounds[3] - self.size)
+
+        self.state[crossed_x1, 0] = self.bounds[0] + self.size
+        self.state[crossed_x2, 0] = self.bounds[1] - self.size
+
+        self.state[crossed_y1, 1] = self.bounds[2] + self.size
+        self.state[crossed_y2, 1] = self.bounds[3] - self.size
+
+        self.state[crossed_x1 | crossed_x2, 2] *= -1
+        self.state[crossed_y1 | crossed_y2, 3] *= -1
+
+ 
+
+
+#------------------------------------------------------------
+# set up initial state
+np.random.seed(0)
+init_state = -0.5 + np.random.random((50, 4))
+
+#choosing the road on which the point is to be dropped
+init_state1 = np.random.random(50)-.5
+
+for i in range(50):
+    if(init_state1[i] <= 0) :  #put on vertical road
+
+        init_state[i,1] *= 4
+        init_state[i,2] = 0
+        init_state[i,3] *= 4
+               
+    else : #put on horizontal road
+        init_state[i,0] *= 4
+        init_state[i,2] *= 4
+        init_state[i,3] = 0
+
+box = ParticleBox(init_state, size=0.04)
+dt = 1. / 30 # 30fps
+
+
+#------------------------------------------------------------
+# set up figure and animation
+fig = plt.figure()
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
+                     xlim=(-3.2, 3.2), ylim=(-2.4, 2.4))
+
+# particles holds the locations of the particles
+particles, = ax.plot([], [], 'bo', ms=6)
+
+# rect is the box edge
+rect = plt.Rectangle(box.bounds[::2],
+                     box.bounds[1] - box.bounds[0],
+                     box.bounds[3] - box.bounds[2],
+                     ec='none', lw=2, fc='none')
+ax.add_patch(rect)
+
+#making roads for the cars
+lines = [[(-.5,-2),(-.5,-.5)],[(-.5,-.5),(-2,-.5)],[(-2,.5),(-.5,.5)],[(-.5,.5),(-.5,2)],[(.5,2),(.5,.5)],[(.5,.5),(2,.5)],[(2,-.5),(.5,-.5)],[(.5,-.5),(.5,-2)]]
+c = np.array([(0, 1, 0, 1),(0, 1, 0, 1),(0, 1, 0, 1),(0, 1, 0, 1),(0, 1, 0, 1),(0, 1, 0, 1),(0, 1, 0, 1),(0, 1, 0, 1)])
+lc = mc.LineCollection(lines, colors=c, linewidths=2)
+ax.add_collection(lc)
+
+def init():
+    """initialize animation"""
+    global box, rect
+    particles.set_data([], [])
+    rect.set_edgecolor('none')
+    return particles, rect
+
+def animate(i):
+    """perform animation step"""
+    global box, rect, dt, ax, fig
+    box.step(dt)
+
+    ms = int(fig.dpi * 2 * box.size * fig.get_figwidth()
+             / np.diff(ax.get_xbound())[0])
+    
+    # update pieces of the animation
+    rect.set_edgecolor('k')
+    particles.set_data(box.state[:, 0], box.state[:, 1])
+    particles.set_markersize(ms)
+    return particles, rect
+
+ani = animation.FuncAnimation(fig, animate, frames=600,
+                              interval=10, blit=True, init_func=init)
+
+
+# save the animation as an mp4.  This requires ffmpeg or mencoder to be
+# installed.  The extra_args ensure that the x264 codec is used, so that
+# the video can be embedded in html5.  You may need to adjust this for
+# your system: for more information, see
+# http://matplotlib.sourceforge.net/api/animation_api.html
+#ani.save('particle_box.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+
+plt.show()
