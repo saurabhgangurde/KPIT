091f546bbaab3e119e4cf74d03fdc6b0aa7a2482
ashwinlele <ashwinlele.2009@gmail.com>
2016-09-28 01:31:54 +0530

    errors

--- KPIT-master/classes.py
+++ KPIT-master/classes.py
@@ -24,14 +24,17 @@ class cluster:
 	def assign_avg_vel(self) :
 		self.avg_vel=sum([x.velocity for x in self.cars])
 		self.avg_vel/=float(len(self.cars))
+		
 	
 
 	def assign_weight(self,d) :
 		self.weight=len(self.cars)/(d/self.avg_vel)
+		return self.weight
+		
 	def __str__(self):
 		# for car in self.cars:
 		# 	print car,
-		string="wight:"+str(self.weight)+"number: "+str(self.cluster_no)+ "avg_vel:"+str(self.avg_vel)
+		string="wight:"+str(self.weight)+"number:here "+str(self.cluster_no)+ "avg_vel:"+str(self.avg_vel)
 		return string
 
 # b=cluster(a,[0,0],0)
@@ -39,14 +42,19 @@ class cluster:
 
 def calc_weight_of_lane(clusters):
 	total_weight=sum([x.weight for x in clusters])
-	avg_weight=total_weight/len(cars)
-	return total_weight, avg_weight
+	'''	
+	sum1=0.0
+	for x in range(0,len(weights)):
+		sum1=sum1+weights[x]
+	#avg_weight=total_weight/len(cars)
+	'''
+	return total_weight#, avg_weight
 '''
 def calc_total_weight(lanes):
 	return 
 '''
 
-def distribute_time(threshold,T0,weight_list):
+def distribute_time(T0,weight_list):		#inlcude threshold here
 	sum_weight=sum(weight_list)
 
 	return [(weight_list[m]/sum_weight)*T0 for m in range(0,4)]
